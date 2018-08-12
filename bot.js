d3cd12176e9861e0da0f3a7564a37c3f1b79a183
NorElden12 <41994129+NorElden12@users.noreply.github.com>
2018-08-12 12:02:22 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -304,6 +304,34 @@ message.channel.sendMessage('**الرجاء الانتظار ريث ما يتم 
 
 
 
+
+
+  client.on('message', async message => {
+  if(message.content.startsWith(prefix + "تقديم")) {
+  await  message.channel.send(`#how-to-submit`)
+    let filter = m => m.author.id === message.author.id
+      var text = '';
+        let sugsa = message.channel.awaitMessages(filter, { max: 1, time: 60000})
+          .then(co => {
+            text = co.first().content
+
+              message.channel.send(`تم حفظ اقتراحك الرجاء انتضار الرد من قبل الاداره`)
+                client.channels.get(" 477096226668412939").send(`${message.author.username}'s sug => ${text}`)
+
+              })
+            }
+          })
+  
+
+
+
+
+
+
+
+
+
+
 client.on('message', message => {
 var prefix = "-";
 const user = message.mentions.users.first();
