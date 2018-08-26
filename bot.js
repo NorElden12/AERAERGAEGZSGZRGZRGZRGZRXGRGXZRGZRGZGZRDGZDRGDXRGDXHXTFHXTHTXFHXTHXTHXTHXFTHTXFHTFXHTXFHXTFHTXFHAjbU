57c3bacc94f79b6787f0333df1470575ffe821e0
NorElden12 <41994129+NorElden12@users.noreply.github.com>
2018-08-26 06:09:44 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1061,7 +1061,7 @@ client.on('message', message => {
             if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
 if (message.content.startsWith('-bcall')){
- if (message.author.id !== ' 471996393351806977 ') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
+ if (message.author.id !== '471996393351806977') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
 message.channel.sendMessage('جار ارسال الرسالة |✅')
 client.users.forEach(m =>{
 m.sendMessage(args)
