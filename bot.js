b3f81783d54afe473e2abb66d265f5a4e10a0150
NorElden12 <41994129+NorElden12@users.noreply.github.com>
2018-08-27 05:27:33 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -467,6 +467,22 @@ message.channel.sendMessage('**الرجاء الانتظار ريث ما يتم 
 
   
 
+client.on('message', message => {
+    var args = message.content.split(/[ ]+/)
+    if(message.content.includes('discord.gg')){
+        message.delete()
+      message.channel.sendMessage("", {embed: {
+        title: "لا تنشر",
+        color: 0x06DF00,
+        description: "يمنع النشر في هذا السيرفر",
+        footer: {
+          text: "By Abo Khalil"
+        }
+      }}).then(msg => {msg.delete(3000)});
+                          }
+
+     
+}); 
 
 
 
