417be89303b8d54a9a372a22f8710e4d8fd70aa1
NorElden12 <41994129+NorElden12@users.noreply.github.com>
2018-10-05 05:09:14 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -453,6 +453,24 @@ var prefix = "-";
 
 
 
+client.on('message', message => {
+    var prefix = "-"
+      if (message.content.startsWith(prefix + 'js')) {
+        let code = message.content.split(" ").slice(2).join(" ")
+        let supportrole = message.guild.member(message.author).roles.find('name', 'Support', '+Support');
+        if(!supportrole) return message.reply('ما عندك الرتبة المطلوبة')
+        if(!code) return message.channel.send(`اكتب الوصف`)
+        let applychannel = message.guild.channels.find(`name`,'codes-js')
+        if(!applychannel) return message.channel.send("ما في روم كتابي")
+        
+        applychannel.send(`@everyone , @here
+         تم النشر بواسطة: ${message.author} \`\`\`js
+  ${code}\`\`\``).then((pp)=> {
+      pp.react("☑")
+      pp.react("❌")
+      pp.react("💓")
+  })
+      }});
 
 
 
