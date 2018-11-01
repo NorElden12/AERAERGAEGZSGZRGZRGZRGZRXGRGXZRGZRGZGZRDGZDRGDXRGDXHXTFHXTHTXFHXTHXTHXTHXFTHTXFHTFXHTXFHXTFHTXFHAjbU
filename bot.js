9ed681f121582b1c6e4bf25180bff9d2df91c8ba
NorElden12 <41994129+NorElden12@users.noreply.github.com>
2018-11-01 10:40:56 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -154,6 +154,7 @@ client.on('message', message => {
 
 
 if(!Discord) var Discord = require('discord.js');
+if(!client) var client = new Discord.Client();
 if(!prefix) var prefix = "-" ; // البرفكس 
 
 var stopReacord = true;
@@ -185,58 +186,6 @@ client.on('raw', raw => {
     var reaction = message.reactions.get( (raw.d.emoji.id ? `${raw.d.emoji.name}:${raw.d.emoji.id}` : raw.d.emoji.name) );
     if (raw.t === 'MESSAGE_REACTION_ADD') return client.emit('messageReactionAdd', reaction, client.users.get(raw.d.user_id));
     if (raw.t === 'MESSAGE_REACTION_REMOVE') return client.emit('messageReactionRemove', reaction, client.users.get(raw.d.user_id));
-  });
-});
-client.on('messageReactionAdd', (reaction, user) => {
-    if(user.id == client.user.id) return;
-    if(!stopReacord) {
-      var done = false;
-      reactionRoles[reaction.message.id] = { role: definedReactionRole, message_id: reaction.message.id, emoji: reaction.emoji};
-      stopReacord =  true;
-      definedReactionRole = null;
-      reaction.message.react(reaction.emoji.name)
-      .catch(err => {done = true; reaction.message.channel.send(`sorry i can't use this emoji but the reaction role done! anyone react will get the role!`)})
-      if(done) reaction.remove(user); 
-    } else {
-      var request = reactionRoles[reaction.message.id];
-      if(!request) return;
-      if(request.emoji.name != reaction.emoji.name) return reaction.remove(user);
-      reaction.message.guild.members.get(user.id).addRole(request.role);
-    }
-}) 
-client.on('messageReactionRemove', (reaction, user) => {
-  if(user.id == client.user.id) return;
-  if(!stopReacord) return;
-  let request = reactionRoles[reaction.message.id];
-  if(!request) return;
-  reaction.message.guild.members.get(user.id).removeRole(request.role);
-
-
-
-
-
-
-
-
-
-client.on('message', message => {
-    var prefix = "-"
-      if (message.content.startsWith(prefix + 'an-')) {
-        let code = message.content.split(" ").slice(2).join(" ")
-        let supportrole = message.guild.member(message.author).roles.find('name', 'ann');
-        if(!supportrole) return message.reply('ما عندك الرتبة المطلوبة')
-        if(!code) return message.channel.send(`اكتب الوصف`)
-        let applychannel = message.guild.channels.find(`name`,'announcements')
-        if(!applychannel) return message.channel.send("ما في روم كتابي")
-        
-        applychannel.send(`@everyone , @here
-         تم النشر بواسطة: ${message.author} 
-  ${code}`).then((pp)=> {
-      pp.react("☑")
-      pp.react("❌")
-      pp.react("💓")
-  })
-      }});
 
 
 
