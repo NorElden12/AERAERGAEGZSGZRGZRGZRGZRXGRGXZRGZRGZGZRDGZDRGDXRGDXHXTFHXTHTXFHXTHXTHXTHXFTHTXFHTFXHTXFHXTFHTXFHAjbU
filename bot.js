83727c2baa8cc323147d87512fc82499df2a91f9
NorElden12 <41994129+NorElden12@users.noreply.github.com>
2018-11-01 10:47:38 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -153,7 +153,6 @@ client.on('message', message => {
 
 
 
-if(!Discord) var Discord = require('discord.js');
 if(!client) var client = new Discord.Client();
 if(!prefix) var prefix = "-" ; // البرفكس 
 
