8957e0c562af05edd735cee07a91387726f1d8e8
NorElden12 <41994129+NorElden12@users.noreply.github.com>
2019-03-09 14:03:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -45,11 +45,104 @@ client.user.setGame(`صيانة`)
 
 
 
+client.on("message", message => { 
+      if(message.content == `${prefix}support`) { 
+        const ListEmbed = new Discord.RichEmbed() 
+        .setTitle('Users with the support role:') 
+        .setDescription(message.guild.roles.get('اي دي الرتبة').members.map(m=>m.user.tag).join('\n')); 
+        message.channel.send(ListEmbed); 
+      }
+   });
 
 
 
 
+    if(message.content.startsWith(prefix + "invite")){
+        let emo = client.emojis.find(em => em.id === `551459239768358913`);
+        message.member.sendMessage(`https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot`);
+        message.channel.send(`${emo} | See Your Direct Messages!`).catch(error => {
+            message.channel.send(`${emo} | Open Your DM!`);
+        });
+    }
 
+
+
+let emojiss = require("node-emoji");//npm i node-emoji
+client.on("message", msg=>{
+if(msg.content.startsWith(`${prefix}setRole`)){
+if(!msg.member.hasPermission("ADMINISTRATOR")) return msg.reply("you don't have permission").then(s => {s.delete(1600);})
+msg.reply("منشن الروم الي تبي فيه التفعيل").then(msgs=>{
+  const filter = response => response.author.id === msg.author.id;
+  msg.channel.awaitMessages(filter, { maxMatches: 1, time: 30000, errors: ['time'] })
+  .then( collected =>{
+    msg.delete();
+    let idC = msg.guild.channels.find(c=>c.id == collected.first().mentions.channels.first().id)
+    if(!idC) return msgs.edit("لم اجد الروم");
+     idC = idC.id;
+     msgs.edit(`${msg.author}, ادخل الايموجي الذي تريدة للتفعيل`)
+const filter = response => response.author.id === msg.author.id;
+msg.channel.awaitMessages(filter, { maxMatches: 1, time: 30000, errors: ['time'] })
+.then( collected =>{
+if(!emojiss.hasEmoji(collected.first().mentions._content)) return msgs.edit("ادخل ايموجي صحيح !");
+newemoji = collected.first().mentions._content;
+msg.delete();
+msgs.edit(`${msg.author}, منشن للرتبة الذي تريدها`)
+const filter = response => response.author.id === msg.author.id;
+msg.channel.awaitMessages(filter, { maxMatches: 1, time: 30000, errors: ['time'] })
+.then( collected =>{
+let roleW = collected.first().mentions.roles.first()
+if(!roleW) {
+  let embed = new Discord.RichEmbed()
+  .setColor("#42f4f4")
+  .setTitle(`:x: - منشن الرتبة `);
+  msg.reply(embed).then( z => z.delete(3000)); return
+};
+let role = msg.guild.roles.find(`name`, roleW.name);
+if(!role) {
+  let embed = new Discord.RichEmbed()
+  .setColor("#42f4f4")
+  .setTitle(`:x: - Could't find \`${roleW.name}\` role.`);
+msg.reply(embed).then( msgs => msgs.delete(3000));
+return
+}
+roleNew = role;
+msgs.edit(`${msg.author}, ادخل النص الذي تريدة`)
+const filter = response => response.author.id === msg.author.id;
+msg.channel.awaitMessages(filter, { maxMatches: 1, time: 30000, errors: ['time'] })
+.then( collected =>{
+stringNew = collected.first().mentions._content;
+let channel = msg.guild.channels.get(idC);
+if(!channel) {
+  let embed = new Discord.RichEmbed()
+  .setColor("#42f4f4")
+  .setTitle(`:x: - Could't find \`${idC}\` Channel.`);
+msg.reply(embed).then( msgs => msgs.delete(3000));
+return
+}
+channel.bulkDelete(100)
+channel.send(`@here || @everyone
+${msg.guild.name}© :arrow_down:
+ 
+${stringNew}
+`).then( msgA =>{
+msgA.react(newemoji).then(()=>{
+  const Ac = (reaction, user) => reaction.emoji.name === newemoji && !user.bot;
+  const Acc = msgA.createReactionCollector(Ac, {time: 120000});
+  Acc.on("collect", r=>{
+  let member = msg.guild.members.get(r.users.last().id);
+  if(!member) return;
+  r.remove(member.user);
+if(member.roles.find(r=>r.name == roleNew.name)) return;
+    member.addRole(roleNew);
+  channel.send(`${member.user}, تم تفعيلك`).then(z => z.delete(1500));
+})})})
+}).catch(e => {console.log(e.message)});  
+}).catch(e => {console.log(e.message)});
+}).catch(e => {console.log(e.message)});
+}).catch(e => {console.log(e.message)});
+})
+///
+}});
 
 
 
